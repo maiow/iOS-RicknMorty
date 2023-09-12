c2c55dd1891ac36aad0ad03a3e6f6ce25fae4a06
maiow <emmaria@yandex.ru>
2023-09-12 11:20:47 +0300

    +All episodes response, Episodes detail view

---
+++ RicknMorty/RicknMorty/Models/APIResponseTypes/RMGetAllEpisodesResponse.swift
@@ -0,0 +1,20 @@
+//
+//  RMGetAllEpisodesResponse.swift
+//  RicknMorty
+//
+//  Created by mivanovskaya on 11.09.2023.
+//
+
+import Foundation
+
+struct RMGetAllEpisodesResponse: Codable {
+    
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    let info: Info
+    let results: [RMEpisode]
+}
